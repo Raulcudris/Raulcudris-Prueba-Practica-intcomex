0ccbd4dd51cd877ecde110c4e929a90d85ecff49
Raul Cudris <raulcudriz@hotmail.com>
2022-11-16 18:53:51 -0500

    Creacion de aplicacion en vs

---
+++ Usuario/Usuario/Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Usuario.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string  codigo_ciente { get; set; }
+        public string usuario { get; set; }
+        public string Nombre { get; set; }
+        public string telefono { get; set; }
+        public string correo_electronico { get; set; }
+        public string n_celular { get; set; }
+        public string tipo_contacto { get; set; }
+
+    }
+}
